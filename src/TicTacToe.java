efd34ac500eddd25a7b7825a7f857b675bbeab86
nadinedelia <nadine.delia@yahoo.com>
2020-09-01 10:12:06 +0200

    refactor

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,12 +9,18 @@ public class TicTacToe {
 				{'-', '+', '-', '+', '-'},
 				{' ', '|', ' ', '|', ' '}};
 		
-		for(char[] row : gameBoard) {
-			for(char c : row) {
-				System.out.print(c);
-			}
-			System.out.println();
-		}
+		
+		printGameBoard(gameBoard);
 	}
+
+		public static void printGameBoard(char[][] gameBoard) {
+			for(char[] row : gameBoard) {
+				for(char c : row) {
+					System.out.print(c);
+				}
+				System.out.println();
+			}
+			
+		}
 
 }
