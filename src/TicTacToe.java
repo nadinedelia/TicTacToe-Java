1916c7a99b7083f783ef52585c3a9e98c063a99f
nadinedelia <nadine.delia@yahoo.com>
2020-09-01 10:40:04 +0200

    renamed pos to playerPos for consistency

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,11 +16,10 @@ public class TicTacToe {
 		
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter your placement (1-9):");
-		int pos = scan.nextInt();
+		int playerPos = scan.nextInt();
 		
-		System.out.println(pos);
 		
-		placePiece(gameBoard, pos, "player");
+		placePiece(gameBoard, playerPos, "player");
 		
 		Random rand = new Random();
 		int cpuPos = rand.nextInt(9) + 1;
